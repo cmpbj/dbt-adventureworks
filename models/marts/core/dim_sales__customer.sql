9ed3e73670cfab4d1dee9ad1a573f07923648ff8
carlos pinheiro <carlos.pinheiro@indicium.tech>
2022-11-07 16:53:43 -0300

    add dim_person__locations model and documentation

--- models/marts/core/dim_sales__customer.sql
+++ models/marts/core/dim_sales__customer.sql
@@ -6,9 +6,9 @@ with
             , customer_territory_id
             , store_id
         from {{ ref('stg_sales__customers') }}
-    ),
+    )
 
-    raw_person as (
+    , raw_person as (
         select
             business_entity_id
             , person_first_name
@@ -16,10 +16,10 @@ with
             , person_last_name    
         
         from {{ ref('stg_person__people_information') }}
-    ), 
+    ) 
 
 
-    customer_people as (
+    , customer_people as (
 
          SELECT 
              customers.customer_id
@@ -31,10 +31,10 @@ with
              , customers.customer_territory_id
         FROM raw_customers as customers
         left join raw_person as person on customers.person_information_id = person.business_entity_id
-    ),
+    )
 
 
-    transform as (
+    , transform as (
         select
             customer_id
             , business_entity_id
@@ -42,9 +42,9 @@ with
             , customer_territory_id
             , concat(person_first_name, ' ', coalesce(person_middle_name, ''), ' ', person_last_name) as full_customer_name
         from customer_people
-    ),
+    )
 
-    final as (
+    , final as (
         select
             {{ dbt_utils.surrogate_key(['customer_id', 'business_entity_id']) }} as customer_sk
             , customer_id
